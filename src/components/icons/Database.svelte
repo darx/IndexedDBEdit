c8b256f40f97fda11770da982fb67c9d9563baf0
Tom <darxaar@gmail.com>
2022-12-15 00:02:00 +0000

    transform date object to string

---
+++ src/components/icons/Database.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let name = "database";
+  export let size = 16;
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  aria-hidden="true"
+  {...$$props}
+>
+  {#if name == "database"}
+    <ellipse cx="12" cy="5" rx="9" ry="3" />
+    <path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3" />
+    <path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5" />
+  {:else if name == "columns"}
+    <path
+      d="M12 3h7a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-7m0-18H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7m0-18v18"
+    />
+  {/if}
+</svg>
--- src/utils/storage/fixtures/getFromCache.js
+++ src/utils/storage/fixtures/getFromCache.js
@@ -1,3 +1,4 @@
 (function () {
-  return window.__INDEXED_DB_EDIT__["${key}"];
+  const data = window.__INDEXED_DB_EDIT__["${key}"];
+  return JSON.parse(JSON.stringify(data));
 })();
